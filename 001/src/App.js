4d0718f26f52448cb62e2e464ea871e7d27743ca
“Salmonmonmasons” <masa8532@colorado.edu>
2021-07-10 17:39:17 -0600

    added Bootstrap: Commence React.js website

--- 001/src/App.js
+++ 001/src/App.js
@@ -1,9 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// or less ideally
+import { Button, FormControl, Container, Form, Card, Accordion, Navbar, Nav, Row,Col} from 'react-bootstrap';
+
 function App() {
   return (
-    <h>Mason L. Salmon</h>
+    <Container>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
+      <br></br>
+
+        <Row>
+          <Col></Col>
+          <Col>
+            <Card>
+              <Card.Header>
+                <Nav variant="tabs" defaultActiveKey="#first">
+                  <Nav.Item>
+                    <Nav.Link href="#first">Active</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link href="#disabled" disabled>
+                      Disabled
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                  With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+        </Row>
+    </Container>
   );
 }
 
